c2ae8bd10b27bf682fafd1e7a075a339aaad7a7a
Nupur <solnupur999.com>
2024-08-16 18:14:53 +0530

    basic

---
+++ Variable.js
@@ -0,0 +1,22 @@
+const accountId = 1445;
+let accountEmail = "nupur@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+accountEmail = "sol@gmail.com";
+accountPassword = "5677";
+accountCity = "surat";
+let accountState; //Undefined
+
+console.log(accountEmail);
+console.log(accountId);
+
+console.table([
+  accountEmail,
+  accountId,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+// {} means scope
---
+++ dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof age);
+console.log(typeof undefined); // undefined data type
+console.log(typeof null); // null is an  object
